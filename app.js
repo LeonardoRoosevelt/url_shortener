14623c80305f7b2d5f9f980ef27f1a87ac94e210
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-09-22 17:44:22 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const port = 3000
+const exphbs = require('express-handlebars')
+
 const app = express()
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('this is url_shortener')
+  res.render('index')
 })
 
 app.listen(port, () => {
